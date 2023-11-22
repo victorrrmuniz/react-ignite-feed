fb0c18979c6627c7d174ba005e318ba87c3f61ba
Victor Muniz <victorrmunizz@gmail.com>
2023-11-22 15:44:20 -0300

    create comment structure

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img className={styles.avatar} src="https://github.com/victorrrmuniz.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authosAndTime}>
+                            <strong>Víctor Muniz</strong>
+                            <time title="11 de maio às 8:13h" dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Muito bom, Devon, parabéns!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+
+        </div>
+    );
+}
