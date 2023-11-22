35220039f38d0edbdd0273d4bcac57afec406443
Victor Muniz <victorrmunizz@gmail.com>
2023-11-22 18:55:38 -0300

    pass parameters dinamically

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,39 @@ import { Post } from './components/Post';
 import './global.css';
 import styles from './App.module.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/victorrrmuniz.png',
+      name: 'Víctor Muniz',
+      role: 'Full Stack Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'paragraph', content: '👉' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-11-22 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/victorrrmuniz.png',
+      name: 'Víctor Teste',
+      role: 'Frontend Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'paragraph', content: '👉' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-11-20 20:00:00')
+  },
+]
+
 function App() {
 
   return (
@@ -14,8 +47,18 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {
+            posts.map(post => {
+              return (
+                <Post 
+                  key={post.id} 
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })
+          }
         </main>
       </div>
     </>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -19,14 +19,10 @@ export function Post() {
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p></p>
+                <p></p>
                 <p>👉{' '}<a href="#">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="#">#novoprojeto</a>{' '}
-                    <a href="#">#nlw</a>{' '}
-                    <a href="#">#rocketseat</a>
-                </p>
+                
             </div>
 
             <form className={styles.commentForm}>
