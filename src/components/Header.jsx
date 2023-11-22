25053d2f464842eb904170aa5651b78a8709f9f0
Victor Muniz <victorrmunizz@gmail.com>
2023-11-22 11:04:49 -0300

    completes header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import styles from './header.module.css';
+import igniteLogo from '../assets/ignite-logo.svg';
 
-console.log(styles)
+console.log(igniteLogo);
 
 export function Header() {
     return (
         <header className={styles.header}>
-            <strong>Ignite Feed</strong>
+            <img src={igniteLogo} alt="Logotipo do Ignite" />
         </header>
     );
 }
