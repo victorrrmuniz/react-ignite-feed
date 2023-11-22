3d93d14579d4eb5d82ed600ab1e9c9e777fb1378
Victor Muniz <victorrmunizz@gmail.com>
2023-11-22 12:17:02 -0300

    ends sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 
 import styles from './Sidebar.module.css';
 
@@ -9,11 +10,14 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1492891651366-a5c2839dc64b?q=80&w=1434&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/60244111?v=4" />
+
                 <strong>Víctor Muniz</strong>
                 <span>Web Developer</span>
 
                 <footer>
                     <a href="#">
+                        <PencilLine size={20}/>
                         Editar seu perfil
                     </a>
                 </footer>
