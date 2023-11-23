0a5bcf109ae3e94941670312917e52c8aa8b04fb
Victor Muniz <victorrmunizz@gmail.com>
2023-11-23 20:20:00 -0300

    create functions to handle events

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,13 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+
+        onDeleteComment(content);
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/victorrrmuniz.png" />
@@ -16,7 +22,7 @@ export function Comment({ content }) {
                             <time title="11 de maio às 8:13h" dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title="Deletar comentário">
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
                             <Trash size={24} />
                         </button>
                     </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,6 +35,10 @@ export function Post({ author, publishedAt, content }) {
         setNewCommentText(event.target.value);
     }
 
+    function deleteComment(comment) {
+        console.log(`deletando o comentário ${comment}`)
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -81,7 +85,11 @@ export function Post({ author, publishedAt, content }) {
             <div className={styles.commentList}>
                 {
                     comments.map(comment => {
-                        return <Comment content={comment} />
+                        return <Comment 
+                                    key={comment} 
+                                    content={comment} 
+                                    onDeleteComment={deleteComment} 
+                                />
                     })
                 }
             </div>
